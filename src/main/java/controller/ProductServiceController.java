3909005ccfa167fba3d8adec5d74a4159e9387fd
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-13 09:32:40 -0500

    Update ProductServiceController

--- src/main/java/controller/ProductServiceController.java
+++ src/main/java/controller/ProductServiceController.java
@@ -4,14 +4,12 @@ package controller;
 import model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class ProductServiceController {
 
     private static Map<String, Product> productRepo = new HashMap<>();
@@ -38,13 +36,21 @@ public class ProductServiceController {
         return new ResponseEntity<>(productRepo.values(), null, 200);
     }
 
+    @RequestMapping(value = "/products/{id}" , method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id , @RequestBody Product product) {
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>(productRepo.values(), null, 200);
+    }
+
     @RequestMapping(value = "/products/" , method = RequestMethod.POST)
     public ResponseEntity<Object> createProduct(@RequestBody Product product) {
         productRepo.put(product.getId(), product);
         return new ResponseEntity<>(productRepo.values(), null, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/products/" , method = RequestMethod.PUT)
+    @RequestMapping(value = "/products/{id}" , method = RequestMethod.PUT)
     public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
         productRepo.remove(id);
         product.setId(id);
