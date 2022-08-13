483aef860a9239d65c88785a1eb6c76ec6c16c06
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-13 08:54:37 -0500

    first commit

---
+++ src/main/java/com/example/restfulwebservicesdemo/RestfulwebservicesdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebservicesdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulwebservicesdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulwebservicesdemo/RestfulwebservicesdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicesdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
